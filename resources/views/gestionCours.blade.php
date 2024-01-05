2ce0885ff07101259e72fb9fbc6d7d60ef31a507
SoraPF <78522622+SoraPF@users.noreply.github.com>
2024-01-04 15:26:50 -1000

    Update gestionCours.blade.php
    add drag and drop in gestionCours view

--- resources/views/gestionCours.blade.php
+++ resources/views/gestionCours.blade.php
@@ -4,6 +4,40 @@
            Gestion de cours
         </h2>
     </x-slot>
+    <style>
+        /* Add some styling for better visualization */
+        .draggable {            
+            padding: 10px;
+            margin: 5px;
+            cursor: move;
+            width: 100px;
+            height: 50px;
+            background-color: lightblue;
+        }
+
+        .dropzone {
+            position: relative;
+            border: 2px dashed #ccc;
+            border-radius: 50%;
+            padding: 10px;
+            margin: 10px 0;
+            width: auto;
+            height: 200px;
+            overflow: hidden;
+            text-align: center;
+        }
+
+        .rectangle {
+        display: inline-block;
+        margin-top: 63px;
+        margin-right: 10px;
+        margin-left: 10px;
+        width: 100px;
+        height: 50px;
+        background-color: #e0e0e0;
+        border: 1px solid #ccc;
+        }
+    </style>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -24,10 +58,74 @@
                                 gestion cours Vendredi
                             </x-primary-button>
                         </form>
-                    </div>                    
+                    </div>
                 </div>
-                
+
+                <!--<div id="draggable1" class="draggable" draggable="true" ondragstart="drag(event)">
+                    <input type="hidden" name="draggedItems[]" value="val1">
+                    val1
+                </div>-->                
+                @if( isset($participant_name))
+                <div class="text-white text-center">Drop items here</div>
+                <div class="dropzone">
+                    <div class="rectangle" ondragover="allowDrop(event)" ondrop="drop(event)"></div>
+                    <div class="rectangle" ondragover="allowDrop(event)" ondrop="drop(event)"></div>
+                    <div class="rectangle" ondragover="allowDrop(event)" ondrop="drop(event)"></div>
+                    <div class="rectangle" ondragover="allowDrop(event)" ondrop="drop(event)"></div>
+                    <div class="rectangle" ondragover="allowDrop(event)" ondrop="drop(event)"></div>
+                    <div class="rectangle" ondragover="allowDrop(event)" ondrop="drop(event)"></div>
+                </div>
+                <div class="text-white">
+                    @foreach($participant_name as $index => $name)
+                        <div id="draggable{{ $index }}" class="draggable" draggable="true" ondragstart="drag(event)">
+                            <input type="hidden" name="draggedItems[]" value="{{$name}}">
+                            {{$name}}
+                        </div>
+                    @endforeach
+                </div>
+                @endif
             </div>
         </div>
     </div>
 </x-app-layout>
+<script>
+    let draggedItem;
+
+    function drag(event) {
+        // Set the data being dragged (in this case, the input value)
+        event.dataTransfer.setData("text/plain", event.target.id);
+        draggedItem = event.target;
+    }
+
+    function allowDrop(event) {
+        // Prevent the default behavior to allow a drop
+        event.preventDefault();
+    }
+
+    function drop(event) {
+        // Prevent the default behavior to allow a drop
+        event.preventDefault();
+
+        // Get the data being dragged
+        var data = event.dataTransfer.getData("text/plain");
+
+        // Append the dragged element to the drop target
+        var droppedElement = document.getElementById(data);
+        event.target.appendChild(droppedElement);
+    }
+
+    function dragEnd() {
+        // Remove the "dragging" class after the drag operation is completed
+        draggedItem.classList.remove('dragging');
+    }
+
+    document.addEventListener('dragstart', function(event) {
+        // Add a "dragging" class to the dragged item during the drag operation
+        event.target.classList.add('dragging');
+    });
+
+    document.addEventListener('dragend', function() {
+        // Remove the "dragging" class after the drag operation is completed
+        dragEnd();
+    });
+</script>
